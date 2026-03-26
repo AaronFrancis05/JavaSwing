89da66a22308e1c1f6e8691559564a17a7b8f3b9
AaronFrancis <aarontaremwa8@gmail.com>
2026-03-26 13:03:42 +0300

    Upadte test

--- Simple/src/test/java/org/example/AppTest.java
+++ Simple/src/test/java/org/example/AppTest.java
@@ -1,17 +1,20 @@
 package org.example;
 
+import org.junit.Test;
+import static org.junit.Assert.*;
 import javax.swing.SwingUtilities;
-import java.awt.*;
-/**
- * Unit test for simple App.
- */
 
 public class AppTest {
-    public static void main(String[] args) {
-        // Run the GUI creation on the Event Dispatch Thread
-        SwingUtilities.invokeLater(() -> {
-            App frame = new App();
-            frame.setVisible(true);
-        });
+
+    @Test
+    public void testAppInitialization() {
+        // This test checks if the App can be initialized without crashing
+        App frame = new App();
+
+        // Assert that the frame was created successfully
+        assertNotNull("The App frame should not be null", frame);
+
+        // Basic check for your JFrame properties
+        assertEquals("Java GUI Example", frame.getTitle());
     }
 }
